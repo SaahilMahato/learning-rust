929be3486144cdc5142d22ab3004aef188e6f0b7
SaahilMahato <saahilmahato72@gmail.com>
2022-02-01 15:14:07 +0545

    placeholder traits number system

--- src/print.rs
+++ src/print.rs
@@ -2,12 +2,20 @@ pub fn run() {
     // Print to console
     println!("Hello from print.rs file");
 
-    println!("Number: {} Another: {}", 1, 2);
-
+    // print by using placeholders
     println!("{} is so {}", "Saahil", "lazy");
 
+    // can print non-string data only by placeholder
+    println!("Number: {} Another: {}", 1, 2);
+
+    // print by placeholder and indexing
     println!("{0} is from {1} and {0} likes to {2}", "Saahil", "Bhaktapur", "code");
 
+    // print by placeholder and named parameters
     println!("{name} like to play {activity}.", name="Saahil", activity="Video games");
+
+
+    // Placeholder traits
+    println!("Binary: {:b}, Hex: {:x}, Octal: {:o}", 10, 10, 10);
 }
 
