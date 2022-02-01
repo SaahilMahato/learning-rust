d13b18fec824c9b19d7942effd66ed1bc18168b5
SaahilMahato <saahilmahato72@gmail.com>
2022-02-01 15:26:38 +0545

    Variables

---
+++ src/vars.rs
@@ -0,0 +1,20 @@
+// Variables hold primitive data or references to data
+// Variable are immutable by default
+// Rust is a block-scoped language
+
+pub fn run() {
+    let name = "Saahil";
+    let mut age = 23; // make variable mutable
+
+    age = 24;
+
+    println!("Name = {}, Age = {}", name, age);
+
+    // Define constants
+    const ID: i32 = 001; // constants need to have their type defined
+    println!("ID: {}", ID);
+
+    // Assign multiple vars
+    let (my_name, my_age) = ("Saahil", 23);
+    println!("Name: {}, Age: {}", my_name, my_age);
+}
