7ade759853807e4b8806912353321bb8d787ec79
SaahilMahato <saahilmahato72@gmail.com>
2022-02-03 18:29:57 +0545

    Vectors

--- src/vectors.rs
+++ src/vectors.rs
@@ -1,3 +1,42 @@
+// Vectors are resizable arrays
+
 pub fn run() {
-    
+    let mut numbers: Vec<i32> = vec![1, 2, 3];
+
+    // Re-assign value
+    numbers[2] = 20;
+
+    //Add on to vector
+    numbers.push(4);
+    numbers.push(5);
+
+    //Pop off last value
+    numbers.pop();
+
+    println!("{:?}", numbers);
+
+    // get sinlge element
+    println!("First element: {}", numbers[0]);
+
+    // get vector length
+    println!("Vector length: {}", numbers.len());
+
+    // Vectors are stack allocated
+    println!("Vector occupies {} bytes", std::mem::size_of_val(&numbers));
+
+    // Get slice
+    let slice: &[i32] = &numbers[0..2];
+    println!("{:?}", slice);
+
+    // Loop through vector values
+    for x in numbers.iter() {
+        println!("Number: {}", x);
+    }
+
+    // Loop & mutate values
+    for x in numbers.iter_mut() {
+        *x *= 2;
+    }
+
+    println!("Numbers: {:?}", numbers);
 }
